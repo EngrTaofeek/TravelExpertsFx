830ce6be6e0b921f1a0e6a02817869bdc546e28f
DarylWxc <darylWxc@outlook.com>
2025-02-21 14:52:05 -0700

    # finish the CURD part of Supplier

---
+++ src/main/java/com/groupfour/travelexpertsfx/utils/AlertMessage.java
@@ -0,0 +1,21 @@
+package com.groupfour.travelexpertsfx.utils;
+
+/**
+ * @Author DarylWxc
+ * @Date 2/21/2025
+ * @Description Alert Class
+ */
+public class AlertMessage {
+    /**
+     * Alert Message to User
+     * @param alertType
+     * @param message
+     */
+    public void alertMessage(javafx.scene.control.Alert.AlertType alertType, String message) {
+        javafx.scene.control.Alert alert = new javafx.scene.control.Alert(alertType);
+        alert.setTitle(alertType.toString());
+        alert.setHeaderText("Header");
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.groupfour.travelexpertsfx {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.desktop;
 
     opens com.groupfour.travelexpertsfx.controllers to javafx.fxml;
     opens com.groupfour.travelexpertsfx.models to javafx.base;
